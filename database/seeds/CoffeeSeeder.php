bb607dac0e0cc5e60a798cf00d8d5e60722e86af
Pablo Villalba <garanaw@gmail.com>
2018-06-20 10:50:27 +0200

    Added coffee seeder

---
+++ database/seeds/CoffeeSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CoffeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+	DB::table('coffees')->truncate();
+	foreach($this->getCoffeeArray() as $coffee){
+	    DB::table('coffees')
+		    ->insert($coffee);
+	}
+    }
+    
+    private function getCoffeeArray(){
+	return [
+	    [
+		'name' => 'White Coffee',
+		'price' => '2.00'
+	    ],
+	    
+	    [
+		'name' => 'Black Coffee',
+		'price' => '1.50'
+	    ],
+	    
+	    [
+		'name' => 'Americano',
+		'price' => '1.50'
+	    ],
+	    
+	    [
+		'name' => 'Latte',
+		'price' => '2.50'
+	    ],
+	    
+	    [
+		'name' => 'Capuccino',
+		'price' => '3.00'
+	    ]
+	];
+    }
+}
+
+
+/*
+ * White Coffee 	2.00
+ 2 Black Coffee 	1.50
+ 3 Americano		1.50
+ 4 Latte		2.50
+ 5 Capuccino		3.00
+ */
