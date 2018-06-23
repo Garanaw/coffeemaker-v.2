126ae8e899450853e86a9dd1514efe26377efe36
Pablo Villalba <garanaw@gmail.com>
2018-06-23 16:20:39 +0200

    Added methods coffee:sumary and coffee:store

--- app/Http/Controllers/CoffeeController.php
+++ app/Http/Controllers/CoffeeController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
-use Illuminate\Http\Request;
+
+use App\Models\Coffee;
+use App\Models\Order;
 
 class CoffeeController extends Controller
 {
@@ -12,5 +14,29 @@ class CoffeeController extends Controller
 	$coffees = DB::table('coffees')->get();
 	
 	return view('coffee', ['coffees' => $coffees]);
+    }
+    
+    public function summary(){
+	$data['order'] = request()->all();
+	
+	$data['order']['coffee'] = Coffee::find($data['order']['coffee']);
+	$data['order']['milk'] = isset($data['order']['milk']) ? $data['order']['milk'] : 0;
+	$data['order']['sugar'] = isset($data['order']['sugar']) ? $data['order']['sugar'] : 0;
+	
+	return view('summary', $data);
+    }
+    
+    public function store() {
+	$data = request()->all();
+	
+	$order = [
+	    'coffee_id' => $data['coffee'],
+	    'milk' => $data['milk'],
+	    'sugar' => $data['sugar']
+	];
+	
+	Order::create($order);
+	
+	return redirect('coffee');
     }
 }
